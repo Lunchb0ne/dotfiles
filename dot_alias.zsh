5f9aa8a8178f1109b92d3d60d5117e7ebdefb540
lunchb0ne <lunchb0ne@users.noreply.github.com>
2026-05-04 00:39:20 +0100

    zsh + niri: idiomatic z4h layout with chezmoi
    zsh:
    - Split .zshrc into framework-only (sources .env.zsh, .alias.zsh)
    - .env.zsh: SSH_AUTH_SOCK, MANPAGER, cursor fix, completion cacher
    - .alias.zsh: eza/bat/pacman aliases, salvaged from cachyos
    - Add atuin shell history with Ctrl+R binding
    - Add zoxide directory jumper
    - Drop redundant cachyos oh-my-zsh config
    - SSH teleport sends .env.zsh and .alias.zsh to remotes
    
    niri:
    - Track hand-edited configs (config.kdl, dms binds/windowrules)
    - Ignore DMS auto-generated files (colors, layout, outputs, profiles)
    - Unmanage DankMaterialShell (machine-generated settings)
    
    housekeeping:
    - Commit .chezmoiignore, ignore .vscode/
    - Clean out old mangoWM configs

---
+++ dot_alias.zsh
@@ -0,0 +1,40 @@
+# Aliases and short functions.
+
+function md() {
+    [[ $# == 1 ]] && mkdir -p -- "$1" && cd -- "$1"
+}
+compdef _directories md
+
+# ---------- eza / ls ----------
+alias tree='tree -a -I .git'
+alias eza='eza --icons=always'
+alias ls="${aliases[ls]:-ls} -A"
+
+# ---------- bat / cat ----------
+if command -v bat >>/dev/null; then
+    alias cat_="$(where cat)"
+    alias cat='bat'
+fi
+
+# ---------- build tools ----------
+alias make="make -j$(nproc)"
+alias ninja="ninja -j$(nproc)"
+alias n='ninja'
+
+# ---------- pacman ----------
+alias update='sudo pacman -Syu'
+alias rmpkg='sudo pacman -Rsn'
+alias cleanup='sudo pacman -Rsn $(pacman -Qtdq)'
+alias cleanch='sudo pacman -Scc'
+alias fixpacman='sudo rm /var/lib/pacman/db.lck'
+
+# ---------- system ----------
+alias c='clear'
+alias please='sudo'
+alias jctl='journalctl -p 3 -xb'
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias tb='nc termbin.com 9999'
+
+# ---------- humor ----------
+alias apt='man pacman'
+alias apt-get='man pacman'
